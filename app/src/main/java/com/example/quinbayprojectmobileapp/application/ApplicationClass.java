7f41cbfc15d651e9e0f0990d2a085fbae93021ed
puneetsharma <puneetsharmaps023@gmail.com>
2023-01-22 15:41:39 +0530

    added application class and updated the dependencies

---
+++ app/src/main/java/com/example/quinbayprojectmobileapp/application/ApplicationClass.java
@@ -0,0 +1,16 @@
+package com.example.quinbayprojectmobileapp.application;
+
+import android.app.Application;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApplicationClass extends Application {
+    public Retrofit retrofit;
+   @Override
+    public void onCreate()
+    {
+        super.onCreate();
+    }
+}
